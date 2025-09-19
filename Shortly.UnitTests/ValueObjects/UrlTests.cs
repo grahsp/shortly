9b724dc06b60a8f20cefc458aae8ce698a9c5c59
grahsp <pontus.grahsp@gmail.com>
2025-09-19 11:15:56 +0200

    test: add unit tests for `Url` value object

---
+++ Shortly.UnitTests/ValueObjects/UrlTests.cs
@@ -0,0 +1,28 @@
+﻿using Shortly.Core.ValueObjects;
+
+namespace Shortly.UnitTests.ValueObjects;
+
+public class UrlTests
+{
+    [Fact]
+    public void Url_Succeeds_OnValidInput()
+    {
+        const string input = "https://www.google.com";
+        
+        var url = new Url(input);
+        
+        Assert.Equal(input, url.ToString());
+    }
+
+    [Fact]
+    public void Url_Throws_OnMissingScheme()
+    {
+        Assert.Throws<ArgumentException>(() => new Url("www.google.com"));
+    }
+    
+    [Fact]
+    public void Url_Throws_OnInvalidScheme()
+    {
+        Assert.Throws<ArgumentException>(() => new Url("ftp://www.google.com"));
+    }
+}
